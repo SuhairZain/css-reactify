30020c4ef7e3351a3d848eb2203e18ce0aecd3d3
SuhairZain <suhairzain11@gmail.com>
2016-05-30 12:20:24 +0530

    styled fields

--- src/InputCard.jsx
+++ src/InputCard.jsx
@@ -9,28 +9,36 @@ import TextField from 'material-ui/TextField';
 
 class InputCard extends React.Component {
     styles = {
-        card: {
+        root: {
             marginLeft: '4px',
             marginRight: '4px',
             width: '50%'
         },
         field: {
-            margin: '8px'
+            padding: '0 4px'
+        },
+        underline: {
+            left: '4px',
+            width: 'calc(100% - 16px)'
         }
     };
 
     render() {
         return (
-            <Card style={this.styles.card}>
-                <TextField
-                    value={this.props.text}
-                    style={this.styles.field}
-                    hintText="MultiLine with rows: 2 and rowsMax: 4"
-                    multiLine={true}
-                    fullWidth={true}
-                    onChange={this.props.onChange}
-                    rows={2}/>
-            </Card>
+            <div style={this.styles.root}>
+                <Card style={this.styles.card}>
+                    <TextField
+                        value={this.props.text}
+                        style={this.styles.field}
+                        hintText="MultiLine with rows: 3 and rowsMax: 4"
+                        multiLine={true}
+                        fullWidth={true}
+                        rows={8}
+                        underlineStyle={this.styles.underline}
+                        style={this.styles.field}
+                        onChange={this.props.onChange}/>
+                </Card>
+            </div>
         );
     }
 }

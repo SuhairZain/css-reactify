857cc123b4c960be7ef854718bd64709de2497ad
SuhairZain <suhairzain11@gmail.com>
2016-05-30 14:13:49 +0530

    added support for vendor prefixed

--- src/core/reactify.js
+++ src/core/reactify.js
@@ -5,6 +5,36 @@
 import camelCase from 'camelcase';
 import {parse} from 'css';
 
+const isVendorPrefixed = function(prop){
+    const prefixes = [
+        'moz', 'ms', 'o', 'webkit'
+    ];
+
+    for(const prefix of prefixes){
+        if(prop.startsWith(prefix) && isUpperCase(prop.charAt(prefix.length))){
+            return true;
+        }
+    }
+    return false;
+};
+
+function isUpperCase(str) {
+    return str !== str !== str.toLowerCase();
+}
+
+const upperCase = function(prop){
+    return prop.charAt(0).toUpperCase() + prop.substr(1, prop.length-1);
+
+};
+
+const upperCaseIfNecessary = function(prop){
+    return prop.startsWith('ms')?prop:upperCase(prop);
+};
+
+const properlyVendorify = function(prop){
+    return isVendorPrefixed(prop)?upperCaseIfNecessary(prop):prop;
+};
+
 export const reactify = function(object){
     const out = {};
 
@@ -12,7 +42,7 @@ export const reactify = function(object){
         const cls = {};
 
         rule.declarations.map(function (declaration) {
-            cls[camelCase(declaration.property)] = declaration.value;
+            cls[properlyVendorify(camelCase(declaration.property))] = declaration.value;
         });
 
         out[camelCase(rule.selectors[0])] = cls;
